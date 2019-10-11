d0316ebe112d1b8e4df13a6e6d3653a54503d5a1
VoxleJ <45022791+VoxleJ@users.noreply.github.com>
2019-10-11 17:09:20 -0400

    Add files via upload

---
+++ FHS_StrainExv1.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  4 11:43:51 2019
+
+@author: vjha1
+"""
+
+
+import pandas as pd
+import numpy as np
+#import iflvisfirst
+#import ifrvisfirst
+#import time
+import lvfirst
+import rvfirst
+
+filename = input('Please enter the filename: ' )
+df = pd.read_csv (filename)
+data1 = df.to_numpy()
+
+LVwhere = np.argwhere(data1 == "Left Ventricle    ")
+
+RVwhere = np.argwhere(data1 == "Right Ventricle     ")
+
+if LVwhere[0,0] > RVwhere [0,0]:
+    print('Detected RV Values First')
+    print()
+#    ifrvisfirst.RVfirst2(filename)
+#    time.sleep(10)
+    rvfirst.RV2(filename)
+    
+
+elif LVwhere[0,0] < RVwhere [0,0]:
+    print('Detected LV Values First')
+    print()
+#    iflvisfirst.LVfirst2(filename)
+#    time.sleep(10)
+    lvfirst.LV2(filename)
+    
---
+++ lvfirst.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 15:59:41 2019
+
+@author: vjha1
+"""
+
+def LV2(filename):
+    import pandas as pd
+    import numpy as np
+    import csv
+
+
+    df = pd.read_csv (filename)
+    data1 = df.to_numpy()
+
+    #Global Results Table RV2D, LV2D, LV3D
+    Globewhere = np.argwhere(data1 == "Global")
+    
+    LVSax = data1[Globewhere[0,0]]
+    LVSax = LVSax.tolist()
+    del LVSax[0:2]
+    #print(RVSax)
+    
+    LVLax = data1[Globewhere[1,0]]
+    LVLax = LVLax.tolist()
+    del LVLax[0:2]
+    #print(RVLax)
+    
+    LV3D = data1[Globewhere[2,0]]
+    LV3D = LV3D.tolist()
+    del LV3D[0:2]
+    #skip data1[Globewhere[2,0]]
+    
+    RVSax = data1[Globewhere[3,0]]
+    RVSax = RVSax.tolist()
+    del RVSax[0:2]
+    #print(LVSax)
+    
+    RVLax = data1[Globewhere[4,0]]
+    RVLax = RVLax.tolist()
+    del RVLax[0:2]
+    #print(LVLax)
+    
+    RV3DRadial = [0]*2
+    RV3DRadialp = data1[Globewhere[18,0]]
+    RV3DRadialp = RV3DRadialp.tolist()
+    RV3DRadialp.pop(0)
+    del RV3DRadialp[20:26]
+    #RV3DRadial1 = np.array(RV3DRadial)
+    #float(RV3DRadial)
+    #print(RV3DRadialp)
+    RV3DRadialp = max(RV3DRadialp, key=float)
+    
+    RV3DRadialrate = data1[Globewhere[19,0]]
+    RV3DRadialrate = RV3DRadialrate.tolist()
+    RV3DRadialrate.pop(0)
+    del RV3DRadialrate[20:26]
+    #print(RV3DRadialrate)
+    RV3DRadialrate = max(RV3DRadialrate, key=float)
+    
+    RV3DRadial = [RV3DRadialp, RV3DRadialrate]
+    
+    RV3DCircum = [0]*2
+    
+    RV3DCircump = data1[Globewhere[22,0]]
+    RV3DCircump = RV3DCircump.tolist()
+    RV3DCircump.pop(0)
+    del RV3DCircump[20:26]
+    RV3DCircump = min(RV3DCircump, key=float)
+    
+    
+    RV3DCircumrate = data1[Globewhere[23,0]]
+    RV3DCircumrate = RV3DCircumrate.tolist()
+    RV3DCircumrate.pop(0)
+    del RV3DCircumrate[20:26]
+    RV3DCircumrate = min(RV3DCircumrate, key=float)
+    
+    RV3DCircum = [RV3DCircump, RV3DCircumrate]
+    
+    RV3DLong = [0]*2
+    
+    RV3DLongp = data1[Globewhere[26,0]]
+    RV3DLongp = RV3DLongp.tolist()
+    RV3DLongp.pop(0)
+    del RV3DLongp[20:26]
+    RV3DLongp = min(RV3DLongp, key=float)
+    
+    RV3DLongrate = data1[Globewhere[27,0]]
+    RV3DLongrate = RV3DLongrate.tolist()
+    RV3DLongrate.pop(0)
+    del RV3DLongrate[20:26]
+    RV3DLongrate = min(RV3DLongrate, key=float)
+    
+    RV3DLong = [RV3DLongp, RV3DLongrate]
+    
+    
+    torsionwhere = np.argwhere(data1 == "torsion")
+    RVTorsion = data1[torsionwhere[1,0]]
+    RVTorsion = RVTorsion.tolist()
+    RVTorsion.pop(0)
+    del RVTorsion[20:26]
+    RVTorsion = max(RVTorsion, key=float)
+    #print(RVTorsion)
+    
+    LVTorsion = data1[torsionwhere[0,0]]
+    LVTorsion = LVTorsion.tolist()
+    LVTorsion.pop(0)
+    #print(LVTorsion)
+    del LVTorsion[20:26]
+    LVTorsion = max(LVTorsion, key=float)
+    
+    #print(LVTorsion)
+    
+    patientwhere = np.argwhere(data1 == "Patient")
+    pnamefull = data1[patientwhere[0,0]]
+    pnamefull = pnamefull.tolist()
+    pnamefull.pop(0)
+    fullname = pnamefull[0]
+    Name = fullname.split(',')
+    
+    birthdatewhere = np.argwhere(data1 == "Birth Date")
+    bdate = data1[birthdatewhere[0,0]]
+    bdate = bdate.tolist()
+    bdate.pop(0)
+    
+    IDwhere = np.argwhere(data1 == "PatientID")
+    pid = data1[IDwhere[0,0]]
+    pid = pid.tolist()
+    pid.pop(0)
+    
+    Sdatewhere = np.argwhere(data1 == "Study Date")
+    Sdate = data1[Sdatewhere[0,0]]
+    Sdate = Sdate.tolist()
+    Sdate.pop(0)
+    
+    #LV Measurements
+    #Search: EDV, ESV, SV,HR,CO,EF,MyoMass_diast
+    
+    edvwhere = np.argwhere(data1 == "EDV")
+    edv = data1[edvwhere[0,0]]
+    edv = edv.tolist()
+    edv.pop(0)
+    
+    esvwhere = np.argwhere(data1 == "ESV")
+    esv = data1[esvwhere[0,0]]
+    esv = esv.tolist()
+    esv.pop(0)
+    
+    svwhere = np.argwhere(data1 == "SV")
+    sv = data1[svwhere[0,0]]
+    sv = sv.tolist()
+    sv.pop(0)
+    
+    hrwhere = np.argwhere(data1 == "HR")
+    hr = data1[hrwhere[0,0]]
+    hr = hr.tolist()
+    hr.pop(0)
+    
+    COwhere = np.argwhere(data1 == "CO")
+    co = data1[COwhere[0,0]]
+    co = co.tolist()
+    co.pop(0)
+    
+    EFwhere = np.argwhere(data1 == "EF")
+    ef = data1[EFwhere[0,0]]
+    ef = ef.tolist()
+    ef.pop(0)
+    
+    LVMwhere = np.argwhere(data1 == "MyoMass_diast") 
+    lvm = data1[LVMwhere[0,0]]
+    lvm = lvm.tolist()
+    lvm.pop(0)
+    
+    #Assemble data into single list
+    procdata = [Name[0], Name[1], Sdate[0], "1","Vibhav","nan", "nan", pid[0], bdate[0], 
+                hr[0], edv[0], esv[0], sv[0], ef[0], co[0], lvm[0], LVLax[2], LVLax[8], 
+                LV3D[2], LV3D[8], LVSax[1], LVSax[7], LV3D[1], 
+                LV3D[7], LVSax[0], LVLax[0], LVSax[6], LVLax[6], 
+                LV3D[0], LV3D[6], LVTorsion, RVLax[2], RVLax[8],RV3DLong[0], 
+                RV3DLong[1], RVSax[1], RVSax[7], RV3DCircum[0], RV3DCircum[1], 
+                RVSax[0], RVLax[0], RVSax[6], RVLax[6], RV3DRadial[0], 
+                RV3DRadial[1], RVTorsion]
+    
+    print(procdata)
+    
+    with open("FHS_autodata.csv", "a", newline="") as fp:
+        writer = csv.writer(fp)
+        writer.writerow(procdata)
